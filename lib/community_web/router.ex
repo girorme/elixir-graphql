b7a88d4a16984a730759b76b12c15c70f8a55275
girorme <rodrigogirorme@gmail.com>
2021-06-03 14:49:24 -0300

    add graphiql endpoint

--- lib/community_web/router.ex
+++ lib/community_web/router.ex
@@ -9,19 +9,12 @@ defmodule CommunityWeb.Router do
     pipe_through :api
   end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  scope "/" do
+    pipe_through :api
 
-    scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-      live_dashboard "/dashboard", metrics: CommunityWeb.Telemetry
-    end
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: CommunityWeb.Schema,
+      interface: :simple,
+      context: %{pubsub: CommunityWeb.Endpoint}
   end
 end
